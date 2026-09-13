16ac748c242c898963705f0fad326265801dd27c
Catherine Roe <clr7695@rit.edu>
2026-09-12 23:06:51 -0400

    testability

--- courseProjectCode/Metrics/README.md
+++ courseProjectCode/Metrics/README.md
@@ -11,3 +11,14 @@
 ### How to run script:
 1. Navigate to the `pandas-SWEN-777` directory
 2. run `python .\courseProjectCode\Metrics/maintainability.py`
+
+
+## Testability
+
+### Calculates the following:
+- Number of Unit Tests
+- Test Coverage
+
+### How to run script:
+1. Navigate to the `pandas-SWEN-777` directory
+2. run `python .\courseProjectCode\Metrics/testability.py`
--- courseProjectCode/Metrics/testability.py
+++ courseProjectCode/Metrics/testability.py
@@ -5,6 +5,7 @@ This file is used to calculate the testability of the pandas core code. It calcu
 """
 
 import pytest
+import coverage
 
 class TestCounterPlugin:
     def __init__(self):
@@ -17,16 +18,20 @@ class TestCounterPlugin:
 
 def main():
     counter = TestCounterPlugin()
+    cov = coverage.Coverage(source=["pandas/core"]) # check the coverage of the core while running the tests
+    cov.start()
 
+    # run tests with counter and cov
     pytest.main([
         ".", # path
-        "--collect-only", # just finds the tests, doesn't run them
-        "-q" # clean output
     ], plugins=[counter])
 
+    cov.stop()
+    cov.save()
+
     print(f"Total test cases found: {counter.test_count}")
-
-
+    print("Code Coverage Report:")
+    cov.report()
 
 
 if __name__ == "__main__":
